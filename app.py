dffff796e545dc4ba7f00d92e23723e126197199
FireBoy48 <gert48750@mail.ru>
2025-04-13 23:57:04 +0300

    try to add exe 1

--- app.py
+++ app.py
@@ -65,5 +65,13 @@ def index():
         selected_lang=selected_lang
     )
 
+
+import webbrowser
+import threading
+
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    threading.Timer(1.0, open_browser).start()
+    app.run(debug=False)
